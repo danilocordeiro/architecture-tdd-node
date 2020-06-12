20afa5668fe4aa0bbeb729e4424587fbb53a2d15
danilocordeiro.dev@gmail.com <danilocordeiro.dev@gmail.com>
2020-06-12 18:23:43 -0300

    Ensure LoginRouter return a valid token if valid credentials are provided

--- src/presentation/routers/login-router.test.js
+++ src/presentation/routers/login-router.test.js
@@ -109,7 +109,8 @@ describe('Login Router', () => {
 
   test('Should return 200 when valid credentials are provided', () => {
     const {
-      sut
+      sut,
+      authUseCaseSpy
     } = makeSut()
 
     const httpRequest = {
@@ -121,6 +122,7 @@ describe('Login Router', () => {
 
     const httpResponse = sut.route(httpRequest)
     expect(httpResponse.statusCode).toBe(200)
+    expect(httpResponse.body.accessToken).toEqual(authUseCaseSpy.accessToken)
   })
 
   test('Should return 500 if AuthUseCase is not provided', () => {
