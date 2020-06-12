f9a72d4dc3734e85e69c598fcc275abe32b61918
danilocordeiro.dev@gmail.com <danilocordeiro.dev@gmail.com>
2020-06-12 17:05:09 -0300

    Ensure LoginRouter return 500 if an when invalid credentials are provided

--- src/presentation/routers/login-router.test.js
+++ src/presentation/routers/login-router.test.js
@@ -102,4 +102,31 @@ describe('Login Router', () => {
     expect(httpResponse.statusCode).toBe(401)
     expect(httpResponse.body).toEqual(new UnauthorizedError())
   })
+
+  test('Should return 500 if AuthUseCase is not provided', () => {
+    const sut = new LoginRouter()
+    const httpRequest = {
+      body: {
+        email: 'any@test.com',
+        password: 'any_password'
+      }
+    }
+
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(500)
+  })
+
+  test('Should return 500 if AuthUseCase has no auth method', () => {
+    const sut = new LoginRouter({})
+
+    const httpRequest = {
+      body: {
+        email: 'any@test.com',
+        password: 'any_password'
+      }
+    }
+
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(500)
+  })
 })
