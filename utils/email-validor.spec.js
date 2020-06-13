46953f9ef567737dfc053f39ecdcc9b664338631
danilocordeiro.dev@gmail.com <danilocordeiro.dev@gmail.com>
2020-06-13 13:58:45 -0300

    ensure EmailVallidator reurns false if validator resturns false

--- utils/email-validor.spec.js
+++ utils/email-validor.spec.js
@@ -1,12 +1,22 @@
+const validator = require('validator')
+
 class EmailValidator {
   isValid (email) {
-    return true
+    return validator.isEmail(email)
   }
 }
 
 describe('Email Validator', () => {
-  test('Should returns true if validator', async () => {})
-  const sut = new EmailValidator()
-  const isEmailValid = sut.isValid('valid_email@test.com')
-  expect(isEmailValid).toBe(true)
+  test('Should returns true if validator returns true', async () => {
+    const sut = new EmailValidator()
+    const isEmailValid = sut.isValid('valid_email@test.com')
+    expect(isEmailValid).toBe(true)
+  })
+
+  test('Should returns false if validator returns false', async () => {
+    validator.isEmailValid = false
+    const sut = new EmailValidator()
+    const isEmailValid = sut.isValid('invalid_email.com')
+    expect(isEmailValid).toBe(false)
+  })
 })
