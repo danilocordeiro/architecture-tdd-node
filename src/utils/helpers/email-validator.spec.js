404bb611572d1ba899b3914abe8c9de351de7770
danilocordeiro.dev@gmail.com <danilocordeiro.dev@gmail.com>
2020-06-14 16:57:23 -0300

    Ensure EmailValidator thorws if no email is provided

--- src/utils/helpers/email-validator.spec.js
+++ src/utils/helpers/email-validator.spec.js
@@ -1,5 +1,8 @@
 const EmailValidator = require('./email-validator')
 const validator = require('validator')
+const {
+  MissingParamError
+} = require('../errors')
 
 const makeSut = () => {
   return new EmailValidator()
@@ -23,5 +26,13 @@ describe('Email Validator', () => {
     const sut = makeSut()
     sut.isValid('valid_email@test.com')
     expect(validator.email).toBe('valid_email@test.com')
+  })
+
+  test('Should throw if no email is provided', async () => {
+    const sut = makeSut()
+
+    expect(() => {
+      sut.isValid()
+    }).toThrow(new MissingParamError('email'))
   })
 })
