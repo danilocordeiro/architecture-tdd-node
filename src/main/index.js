d2b069e9c4f95ff2578cb107376bfb9326c96410
danilocordeiro.dev@gmail.com <danilocordeiro.dev@gmail.com>
2020-06-21 13:01:23 -0300

    Connect mongodb before app listen

--- src/main/index.js
+++ src/main/index.js
@@ -1,5 +1,11 @@
 const app = require('./config/app')
+const MongoHelper = require('../infra/helpers/mongo-helpers')
+const env = require('./config/env')
 
-app.listen(8090, () => {
-  console.log('Server is running')
-})
+MongoHelper.connect(env.mongoUrl)
+  .then(() => {
+    app.listen(8090, () => {
+      console.log('Server is running')
+    })
+  })
+  .catch(console.error)
